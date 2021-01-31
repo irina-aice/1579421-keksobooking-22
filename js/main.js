1ff8b6aeb1020f14701de65c97c69aede1d09091
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-01-31 17:46:02 +0300

    Доработка функций

--- js/main.js
+++ js/main.js
@@ -1,36 +1,19 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getRandomInteger = function (min, max) {
+const getRandom = function (min, max, precision = 0) {
   if (min < 0 || max < 0) {
-    alert('error');
-    return;
+    throw new Error('Числа должны быть больше или равны 0');
   }
 
   if (min > max || min === max) {
-    alert('error');
-    return;
+    throw new Error('Числа не должны быть равны друг другу и максимальное число должно быть больше минимального');
   }
 
-  //https://learn.javascript.ru/number#sluchaynoe-tseloe-chislo-ot-min-do-max
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
-getRandomInteger(10, 11);
-
-
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-const getRandomFloat = function (min, max, precision) {
-  if (min < 0 || max < 0) {
-    alert('error');
-    return;
-  }
-
-  if (min > max || min === max) {
-    alert('error');
-    return;
+  if (!precision) {
+    //https://learn.javascript.ru/number#sluchaynoe-tseloe-chislo-ot-min-do-max
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
   }
 
   //https://learn.javascript.ru/number#sluchaynoe-tseloe-chislo-ot-min-do-max
   let rand = min + Math.random() * (max - min);
   return +rand.toFixed(precision);
 }
-getRandomFloat(1.1, 1.2, 2);
