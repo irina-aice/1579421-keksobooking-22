3cfbc3fe84e1533da259423a11704b2aa2a86207
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-02-23 21:56:37 +0300

    Помощь друга (часть 2)

--- js/adv-form.js
+++ js/adv-form.js
@@ -6,6 +6,8 @@ const typeSelect = document.querySelector('#type');
 const priceInput = document.querySelector('#price');
 const timeinSelect = document.querySelector('#timein');
 const timeoutSelect = document.querySelector('#timeout');
+const roomNumberSelect = document.querySelector('#room_number');
+const capacitySelect = document.querySelector('#capacity');
 
 form.classList.add('ad-form--disabled');
 formElements.forEach((formElement) => {
@@ -14,18 +16,24 @@ formElements.forEach((formElement) => {
 
 typeSelect.addEventListener('change', () => {
   let placeholder;
+  let min;
 
   if (typeSelect.value === 'bungalow') {
     placeholder = '0';
+    min = 0;
   } else if (typeSelect.value === 'flat') {
     placeholder = '1000';
+    min = 1000;
   } else if (typeSelect.value === 'house') {
     placeholder = '5000';
+    min = 5000;
   } else if (typeSelect.value === 'palace') {
     placeholder = '10000';
+    min = 10000;
   }
 
   priceInput.placeholder = placeholder;
+  priceInput.setAttribute('min', min);
 });
 
 timeinSelect.addEventListener('change', () => {
@@ -43,3 +51,42 @@ map.addHandler('load', function () {
     formElement.removeAttribute('disabled');
   });
 })
+
+const checkCapacity = function () {
+  let customValidityMessage = '';
+
+  if (roomNumberSelect.value === '1') {
+    if (capacitySelect.value === '1') {
+      customValidityMessage = '';
+    } else {
+      customValidityMessage = 'Для 1 комнаты допустимое кол-во мест "для 1 гостя"';
+    }
+  } else if (roomNumberSelect.value === '2') {
+    if (capacitySelect.value === '1' || capacitySelect.value === '2') {
+      customValidityMessage = '';
+    } else {
+      customValidityMessage = 'Для 2 комнат допустимое кол-во мест "для 1 гостя" или "для 2 гостей"';
+    }
+  } else if (roomNumberSelect.value === '3') {
+    if (capacitySelect.value === '1' || capacitySelect.value === '2' || capacitySelect.value === '3') {
+      customValidityMessage = '';
+    } else {
+      customValidityMessage = 'Для 3 комнат допустимое кол-во мест "для 1 гостя" или "для 2 гостей" или "для 3 гостей"';
+    }
+  } else if (roomNumberSelect.value === '100') {
+    if (capacitySelect.value === '0') {
+      customValidityMessage = '';
+    } else {
+      customValidityMessage = 'Для 100 комнат допустимое кол-во мест "не для гостей"';
+    }
+  }
+
+  capacitySelect.setCustomValidity(customValidityMessage);
+}
+
+checkCapacity();
+
+roomNumberSelect.addEventListener('change', checkCapacity);
+capacitySelect.addEventListener('change', checkCapacity);
+
+
